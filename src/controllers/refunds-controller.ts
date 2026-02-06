aa257c2a2bbfedd92ebca184b074ac6fb7761516
gustavooeloi <gustavooeloi@gmail.com>
2026-02-05 22:43:29 -0300

    feat: create found route

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,8 +1,48 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+
+const enumCategory = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accomodation",
+]);
 
 class RefundsController {
   async create(request: Request, response: Response) {
-    return response.json("Enviando!");
+    const bodySchema = z.object({
+      name: z
+        .string("Name is required")
+        .trim()
+        .min(3, "Name must have minimum of 3 caracteres"),
+      amount: z
+        .number("Amount is required")
+        .positive("Amount must be higher than 0."),
+      category: enumCategory,
+      filename: z.string(),
+    });
+
+    const { name, amount, category, filename } = bodySchema.parse(request.body);
+
+    if (!request.user) {
+      throw new AppError("Unauthorized", 401);
+    }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        amount,
+        category,
+        filename,
+        userId: request.user.id,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
 
