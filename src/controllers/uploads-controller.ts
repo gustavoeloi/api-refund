26a9eaacc9e2346bedf8b3435da551ccbd6b7728
Gustavo Eloi <gustavooeloi@gmail.com>
2026-02-10 11:09:53 -0300

    feat: manipulating files

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,8 +1,48 @@
 import { Request, Response } from "express";
+import { z, ZodError } from "zod";
+
+import uploadConfig from "@/configs/upload";
+import { AppError } from "@/utils/AppError";
+
+import { DiskStorage } from "@/providers/disk-storage";
 
 class UploadsController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "OK" });
+  async create(request: Request, response: Response) {
+    const diskStorage = new DiskStorage();
+
+    try {
+      const fileSchema = z
+        .object({
+          filename: z.string().min(1, "File is required"),
+          mimetype: z
+            .string()
+            .refine(
+              (type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type),
+              `Invalid Format. Allowed file formart: ${uploadConfig.ACCEPTED_IMAGE_TYPES}`,
+            ),
+          size: z
+            .number()
+            .positive()
+            .refine(
+              (size) => size <= uploadConfig.MAX_FILE_SIZE,
+              `The file exceeds the allowed size, which is ${uploadConfig.MAX_FILE_SIZE} mb`,
+            ),
+        })
+        .loose();
+
+      const file = fileSchema.parse(request.file);
+
+      const filename = await diskStorage.saveFile(file.filename);
+
+      response.json({ filename });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        if (request.file) {
+          await diskStorage.deleteFile(request.file.filename, "tmp");
+        }
+        throw new AppError(error.issues[0].message);
+      }
+    }
   }
 }
 
