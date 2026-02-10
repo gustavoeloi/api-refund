48ffe6cc430513935bb949e9c163e38d902ff824
Gustavo Eloi <gustavooeloi@gmail.com>
2026-02-10 11:12:07 -0300

    feat: preview image on route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import express from "express";
 import cors from "cors";
 import { errorHandling } from "./middlewares/error-handling";
+import uploadConfig from "@/configs/upload";
 
 // routes
 import { routes } from "./routes";
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
